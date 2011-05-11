478103821ff36da85702f276f159653c7cee22c9
Nguyen Duc Khoi <ndkhoi168@gmail.com>
2011-05-12 01:21:26 +0700

    Get current Username

--- script/data.js
+++ script/data.js
@@ -1,18 +1,21 @@
-var currentUser;
 var json;
 
 // Send a request to get viewer id
 function request() {
+  var opts = {};
+  opts[opensocial.DataRequest.PeopleRequestFields.PROFILE_DETAILS] = [
+    opensocial.Person.Field.PROFILE_URL,
+    opensocial.Person.Field.THUMBNAIL_URL];
   var req = opensocial.newDataRequest();
-  req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.OWNER), "get_viewer");
+  req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER, opts), "viewer");
   req.send(response);
 }
 
 function response(dataResponse) {
-  var viewer = dataResponse.get('get_viewer').getData();
-  console.log(viewer);
-  currentUser = "khoi_nguyen";
-  reloadData(currentUser);
+  this.viewer = dataResponse.get('viewer').getData();
+  var profileTempUrl = this.viewer.getField(opensocial.Person.Field.PROFILE_URL);
+  var username = profileTempUrl.substr(profileTempUrl.lastIndexOf('/') + 1)
+  reloadData(username);
 }
 
 // Get the data in rest service
